55d0223293a43d1ad2fbc9c69f57307666650211
ilyasng <127138284+ilyasng@users.noreply.github.com>
2023-12-15 01:18:08 +0100

    Update login_information.dart

--- login_information.dart
+++ login_information.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/gestures.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fluent_ui;
@@ -15,108 +14,135 @@ String generateRandomPassword() {
 
   return password;
 }
-class Password extends StatelessWidget {
-  final Color myColor;
-  final bool obscureText;
-  TextEditingController passwordController = TextEditingController();
 
-  Password({required this.myColor ,  required this.obscureText});
+class Password extends StatefulWidget {
+  final Color myColor;
+
+  Password({required this.myColor});
+
+  @override
+  _PasswordState createState() => _PasswordState();
+}
+
+class _PasswordState extends State<Password> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: myColor,
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-
-              SizedBox(height: 55),
-              Text(
-                "Your Informations *",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        backgroundColor: widget.myColor,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 55),
+            Text(
+              "Your Informations *",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              width: 100 * 0.8,
+              height: 100 * 0.8,
+              child: Image.asset("assets/images/op.png"),
+            ),
+            SizedBox(height: 40),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40.0),
+                child: Text(
+                  "Your Pseudo :",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              SizedBox(height: 25),
-              Container(
-                width: 100 * 0.8,
-                height: 100 * 0.8,
-                child: Image.asset("assets/images/op.png"),
-              ),
-              SizedBox(height: 40),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 40.0),
-                  child: Text(
-                    "Your Pseudo :",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40.0),
+                child: Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 0.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Your Password:",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            if (_obscureText) // Afficher les cercles à l'état initial
+                              Row(
+                                children: List.generate(
+                                  8,
+                                      (index) => Container(
+                                    width: 12,
+                                    height: 12,
+                                    margin: EdgeInsets.symmetric(horizontal: 2),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            if (!_obscureText) // Afficher le mot de passe généré lorsque visible
+                              Text(
+                                generateRandomPassword(),
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            SizedBox(width: 35),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              child: Icon(
+                                _obscureText ? Icons.visibility_off : Icons.visibility,
+                                size: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
+
+
+                  ],
                 ),
               ),
-              SizedBox(height: 20), // Add some spacing between the two text widgets
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 40.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Your Password:",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-
-                      ),
-                      SizedBox(width: 8), // Add spacing between label and password
-                      Text(
-                        generateRandomPassword(),
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.normal,
-                        ),
-                        //
-                      ),
-                      SizedBox(width: 35),
-                      GestureDetector(
-                        onTap: () {
-
-                        },
-                        child: Icon(
-                          Icons.visibility_off,
-                          size: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-
-
-
-                    ],
-                  ),
-                ),
-              ),
-
-
-
-            ],
-          ),
+            ),
+          ],
         ),
-        bottomNavigationBar: BottomAppBar(
-          color: myColor,
-          height: 55,
-          child: Container(
-            color: myColor,
-          ),
-        )
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: widget.myColor,
+        height: 55,
+        child: Container(
+          color: widget.myColor,
+        ),
+      ),
     );
   }
 }
